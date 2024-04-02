0c9631490ff3df6d9cb1800d038f429253111e59
pgxtips <brandongill123@gmail.com>
2024-04-02 20:36:57 +0100

    fix: updated config so when having no .env but having os env is okay

--- app/internal/config/config.go
+++ app/internal/config/config.go
@@ -12,16 +12,38 @@ func ConfigInit(){
     err := godotenv.Load()
 
     if err != nil {
-        log.Fatal("Error loading .env file")
+        log.Println("Error loading .env file")
     }
 
-    os.Setenv("APP_PORT", os.Getenv("APP_PORT"))
-    os.Setenv("DB_HOST", os.Getenv("DB_HOST"))
-    os.Setenv("DB_PORT", os.Getenv("DB_PORT"))
-    os.Setenv("DB_NAME", os.Getenv("DB_NAME"))
-    os.Setenv("DB_USER", os.Getenv("DB_USER"))
-    os.Setenv("DB_PASSWORD", os.Getenv("DB_PASSWORD"))
-    os.Setenv("DB_SSLMODE", "require")
+    port := os.Getenv("APP_PORT")
+    if port == "" { log.Fatal("APP_PORT is not set") }
+
+    dbHost := os.Getenv("DB_HOST")
+    if dbHost == "" { log.Fatal("DB_HOST is not set") }
+
+    dbPort := os.Getenv("DB_PORT")
+    if dbPort == "" { log.Fatal("DB_PORT is not set") }
+
+    dbName := os.Getenv("DB_NAME")
+    if dbName == "" { log.Fatal("DB_NAME is not set") }
+
+    dbUser := os.Getenv("DB_USER")
+    if dbUser == "" { log.Fatal("DB_USER is not set") }
+
+    dbPassword := os.Getenv("DB_PASSWORD")
+    if dbPassword == "" { log.Fatal("DB_PASSWORD is not set") }
+
+    dbSslmode := os.Getenv("DB_SSLMODE")
+    if dbSslmode == "" { log.Fatal("DB_SSLMODE is not set") }
+
+    os.Setenv("APP_PORT", port)
+    os.Setenv("DB_HOST", dbHost)
+    os.Setenv("DB_PORT", dbPort)
+    os.Setenv("DB_NAME", dbName)
+    os.Setenv("DB_USER", dbUser)
+    os.Setenv("DB_PASSWORD", dbPassword)
+    os.Setenv("DB_SSLMODE", dbSslmode)
+
 }
 
 
